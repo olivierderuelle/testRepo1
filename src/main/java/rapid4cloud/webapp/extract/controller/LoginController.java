1521a8fa8fa1c42d4921f5486f5116e83b2341f7
olivier deruelle <olivier.deruelle@rapid4cloud.com>
2018-01-03 15:20:41 +0700

    A

--- src/main/java/rapid4cloud/webapp/extract/controller/LoginController.java
+++ src/main/java/rapid4cloud/webapp/extract/controller/LoginController.java
@@ -1,12 +1,8 @@
 package rapid4cloud.webapp.extract.controller;
 
 import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.endpoint.InfoEndpoint;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
